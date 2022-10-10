81388c80ff9927639bec54f8d7ee8a2da655ef52
Mohammed Ahmed <mahme029@gold.ac.uk>
2022-10-10 15:16:24 +0100

    console message has been added

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express=require("express");
 
 const app=express();
 
-app.get("/",function(req){
+app.get("/",function(req,res){
     res.sendFile(__dirname+"/index.html")
+    console.log("file has been updated")
 })
 
 app.use(express.static("public"))
